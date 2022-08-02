73fe3167e318c129d55a8b487e723e5d240fcc63
Paaz_ <felke2101@gmail.com>
2022-08-02 11:13:25 +0200

    feature(all) More in description
    - Adding button interaction classes on dice container.
    - Add reload button
    - Add somes styles

---
+++ script.js
@@ -0,0 +1,19 @@
+//#####################
+// DÉS:
+//
+//  <i class="fa-solid fa-dice-one"></i>
+//  <i class="fa-solid fa-dice-two"></i>
+//  <i class="fa-solid fa-dice-three"></i>
+//  <i class="fa-solid fa-dice-four"></i>
+//  <i class="fa-solid fa-dice-five"></i>
+//  <i class="fa-solid fa-dice-six"></i>
+//#####################
+
+//######DICE######
+let dice = document.querySelector("dice");
+
+//####PLAYERONE####
+let score1 = document.querySelector("score1");
+let point1 = document.querySelector("point1");
+
+//####PLAYERTWO####
