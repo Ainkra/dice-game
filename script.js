96347e9a79afe8565c5d2c968fca3538b789c36d
Paaz_ <felke2101@gmail.com>
2022-08-26 09:46:23 +0200

    fix() Statement error
    Statement error because, the game actually created permit to play turn by turn. We must allow the player to take a value from the dice

--- script.js
+++ script.js
@@ -14,30 +14,111 @@ let players = [
     document.querySelector(".player2")
 ];
 
-let scores = [
+var scores = [
     document.querySelector(".score1"),
     document.querySelector(".score2")
 ];
 
-let points = [
+var points = [
     document.querySelector(".point1"),
     document.querySelector(".point2")
 ]
 
+var playerTurn = 0;
+
 let reload = document.querySelector(".reload").addEventListener("click", () => {
     Init();
+    console.log("La partie a bien été redémarrée !");
 });
 
-
-
 function Init() {
-    players[0].innerHTML = 0;
-    players[1].innerHTML = 0;
     scores[0].innerHTML  = 0;
     scores[1].innerHTML  = 0;
     points[0].innerHTML  = 0;
     points[1].innerHTML  = 0;
+    playDice.innerHTML = "?"
 }
+
+let playDice = document.querySelector("#playdice");
+
+playDice.addEventListener("click", () => {
+    console.log("cliqué !")
+    turnPlayerHandler();
+
+    let diceRandomizerHandler = Math.floor(Math.random() * 7);
+    console.log(`Le dé à été lancé ! On obtient: ${diceRandomizerHandler}`);
+
+    if(playerTurn === 0) {
+        switch(diceRandomizerHandler) {
+            case 1:
+                playDice.innerHTML = '<i class="fa-solid fa-dice-one"></i>';
+                scores[0].innerHTML = `${+1}`;
+                break;
+            case 2:
+                playDice.innerHTML = '<i class="fa-solid fa-dice-two"></i>';
+                scores[0].innerHTML = `${+2}`;
+                break;
+            case 3:
+                playDice.innerHTML = '<i class="fa-solid fa-dice-three"></i>';
+                scores[0].innerHTML = `${+3}`;
+                break;
+            case 4:
+                playDice.innerHTML = '<i class="fa-solid fa-dice-four"></i>';
+                scores[0].innerHTML = `${+4}`;
+                break;
+            case 5:
+                playDice.innerHTML = '<i class="fa-solid fa-dice-five"></i>';
+                scores[0].innerHTML = `${+5}`;
+                break;
+            case 6:
+                playDice.innerHTML = '<i class="fa-solid fa-dice-six"></i>';
+                scores[0].innerHTML = `${+6}`;
+                break;
+        }
+
+    } else if(playerTurn === 1) {
+            switch(diceRandomizerHandler) {
+                case 1:
+                    playDice.innerHTML = '<i class="fa-solid fa-dice-one"></i>';
+                    scores[1].innerHTML = `${+1}`;
+                    break;
+                case 2:
+                    playDice.innerHTML = '<i class="fa-solid fa-dice-two"></i>';
+                    scores[1].innerHTML = `${+2}`;
+                    break;
+                case 3:
+                    playDice.innerHTML = '<i class="fa-solid fa-dice-three"></i>';
+                    scores[1].innerHTML = `${+3}`;
+                    break;
+                case 4:
+                    playDice.innerHTML = '<i class="fa-solid fa-dice-four"></i>';
+                    scores[1].innerHTML = `${+4}`;
+                    break;
+                case 5:
+                    playDice.innerHTML = '<i class="fa-solid fa-dice-five"></i>';
+                    scores[1].innerHTML = `${+5}`;
+                    break;
+                case 6:
+                    playDice.innerHTML = '<i class="fa-solid fa-dice-six"></i>';
+                    scores[1].innerHTML = `${+6}`;
+                    break;
+            }
+    }
+})
+
+
+function turnPlayerHandler() {
+    if(playerTurn !== 0) {
+        playerTurn = 0;
+        console.log("Au tour du premier joueur !")
+    } else if(playerTurn !== 1) {
+        playerTurn = 1;
+        console.log("Au tour du deuxième joueur !")
+    }
+
+    console.log(playerTurn)
+}
+
 
 
 
